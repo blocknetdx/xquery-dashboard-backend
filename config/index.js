6c5109e0fce158aaf768c1f9e927e002b79d7143
sh_rn <shrnkld@pm.me>
2022-11-22 14:33:15 +0100

    change testing snode IP

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const NEW_PROJECT_MSG = 'XQuery uses a cryptographic signature to verify that you are the owner of this Ethereum address. Please sign this tx to login.'
 const NODE = 'https://bsc-dataseed1.binance.org'
-const SNODE_ENDPOINT = 'http://185.182.184.238/xrs/projects'
+const SNODE_ENDPOINT = 'http://194.163.143.68/xrs/projects'
 
 module.exports = {
   NEW_PROJECT_MSG,
